1d86e9e3cd5364c4c9d5dcb20686330e7ba36b97
Athila <athmatias@gmail.com>
2022-02-22 17:34:23 -0700

    chore: expose more components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
 export { Autocomplete } from './components/Autocomplete';
+export { Button } from './components/Button';
+export { Form } from './components/Form';
+export { ThemeProvider } from './components/ThemeProvider';
 
 export * from './types/index';
